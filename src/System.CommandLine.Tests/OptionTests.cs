KathleenDollard/System.CommandLine

Added ability to specify option argument and assume expected option.

src/System.CommandLine.Tests/OptionTests.cs
@@ -1,6 +1,8 @@
 // Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.CommandLine.Builder;
+using System.Linq;
 using FluentAssertions;
 using Xunit;
 
@@ -142,5 +144,97 @@ namespace System.CommandLine.Tests
                   .Should()
                   .BeEquivalentTo("-h", "--help", "/?");
         }
+
+        [Fact]
+        public void When_option_argument_is_provided_without_option_name_argument_position_is_assumed()
+        {
+            var result = new ParserBuilder()
+                .AddOption("-a", "", a => a.ExactlyOne())
+                .Build()
+                .Parse("value-for-a");
+
+            result.ValueForOption("-a").Should().Be("value-for-a");
+        }
+
+        [Fact]
+        public void When_multiple_option_arguments_are_provided_without_option_name_argument_positions_are_assumed()
+        {
+            var result = new ParserBuilder()
+                .AddOption("-a", "", a => a.ExactlyOne())
+                .AddOption("-b", "")
+                .AddOption("-c", "", a => a.ExactlyOne())
+                .Build()
+                .Parse("value-for-a value-for-c");
+
+            result.ValueForOption("-a").Should().Be("value-for-a");
+            result.ValueForOption("-c").Should().Be("value-for-c");
+            result.HasOption("-b").Should().BeFalse();
+        }
+
+        [Fact]
+        public void When_multiple_option_arguments_are_provided_with_first_option_name_argument_positions_are_assumed()
+        {
+            var result = new ParserBuilder()
+                .AddOption("-a", "", a => a.ExactlyOne())
+                .AddOption("-b", "")
+                .AddOption("-c", "", a => a.ExactlyOne())
+                .Build()
+                .Parse("-a value-for-a value-for-c");
+
+            result.ValueForOption("-a").Should().Be("value-for-a");
+            result.ValueForOption("-c").Should().Be("value-for-c");
+            result.HasOption("-b").Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData("-")]
+        [InlineData("--")]
+        [InlineData("/")]
+        public void When_option_use_differnt_prefixes_they_still_work(string prefix)
+        {
+            var result = new ParserBuilder()
+                .AddOption(prefix + "a", "", a => a.ExactlyOne())
+                .AddOption(prefix + "b", "")
+                .AddOption(prefix + "c", "", a => a.ExactlyOne())
+                .Build()
+                .Parse(prefix + "c value-for-c " + prefix + "a value-for-a");
+
+            result.ValueForOption(prefix + "a").Should().Be("value-for-a");
+            result.ValueForOption(prefix + "c").Should().Be("value-for-c");
+            result.HasOption(prefix + "b").Should().BeFalse();
+        }
+
+        [Fact]
+        public void When_multiple_option_arguments_are_provided_with_second_option_is_first_positions_are_assumed()
+        {
+            var result = new ParserBuilder()
+                .AddOption("-a", "", a => a.ExactlyOne())
+                .AddOption("-b", "")
+                .AddOption("-c", "", a => a.ExactlyOne())
+                .Build()
+                .Parse("-c value-for-c value-for-a");
+
+            result.ValueForOption("-a").Should().Be("value-for-a");
+            result.ValueForOption("-c").Should().Be("value-for-c");
+            result.HasOption("-b").Should().BeFalse();
+        }
+
+        [Fact]
+        public void When_multiple_option_arguments_are_provided_with_b_option_positions_are_assumed()
+        {
+            var result = new ParserBuilder()
+                .AddCommand("outer", "")
+                .AddOption("-a", "", a => a.ExactlyOne())
+                .AddOption("-b", "")
+                .AddOption("-c", "", a => a.ExactlyOne())
+                .Build()
+                .Parse("-c value-for-c -b value-for-a outer");
+
+            result.ValueForOption("-a").Should().Be("value-for-a");
+            result.ValueForOption("-c").Should().Be("value-for-c");
+            result.HasOption("-b").Should().BeTrue();
+            result.Symbols.ElementAt(0).Name.Should().Be("outer");
+        }
+
     }
 }

src/System.CommandLine/Parser.cs
@@ -31,6 +31,8 @@ namespace System.CommandLine
             var errors = new List<ParseError>(lexResult.Errors);
             var unmatchedTokens = new List<string>();
 
+            var optionQueue = GatherOptions(SymbolDefinitions).ToList();
+
             while (unparsedTokens.Any())
             {
                 var token = unparsedTokens.Dequeue();
@@ -66,20 +68,46 @@ namespace System.CommandLine
 
                 var added = false;
 
-                foreach (var parsedOption in Enumerable.Reverse(allSymbols))
+                foreach (var topLevelSymbol in Enumerable.Reverse(allSymbols))
                 {
-                    var option = parsedOption.TryTakeToken(token);
+                    Symbol option = topLevelSymbol.TryTakeToken(token);
 
                     if (option != null)
                     {
                         allSymbols.Add(option);
                         added = true;
+                        if (token.Type is TokenType.Option)
+                        {
+                            var existing = optionQueue.FirstOrDefault(name => name == option.Name);
+                            if (existing != null)
+                            {
+                                // we've used this option - don't use it again
+                                optionQueue.Remove(existing);
+                            }
+                        }
                         break;
                     }
 
                     if (token.Type == TokenType.Argument &&
-                        parsedOption.SymbolDefinition is CommandDefinition)
+                        topLevelSymbol.SymbolDefinition is CommandDefinition)
                     {
+                        var optionName = optionQueue.FirstOrDefault();
+                        if (optionName != null)
+                        {
+                            optionQueue.RemoveAt(0);
+                            var newToken = new Token("-" + optionName, TokenType.Option);
+                            option = topLevelSymbol.TryTakeToken(newToken);
+                            if (option != null)
+                            {
+                                allSymbols.Add(option);
+                                option = topLevelSymbol.TryTakeToken(token);
+                                if (option != null)
+                                {
+                                    allSymbols.Add(option);
+                                    added = true;
+                                }
+                            }
+                        }
                         break;
                     }
                 }
@@ -104,6 +132,29 @@ namespace System.CommandLine
                 unmatchedTokens,
                 errors,
                 rawInput);
+        }
+
+        private IEnumerable<string> GatherOptions(SymbolDefinitionSet symbolDefinitions)
+        {
+            var optionList = new List<string>();
+            foreach (var symDef in symbolDefinitions) //.Where( s => s is OptionDefinition))
+            {
+                if (symDef is OptionDefinition)
+                {
+                    var validator = symDef.ArgumentDefinition.SymbolValidators.FirstOrDefault();
+                    if (validator != null)
+                    {
+                        if (validator.Method.Name.Contains("ExactlyOne"))
+                        {
+                            optionList.Add(symDef.Name);
+                        }
+                    }
+                }
+
+                optionList.AddRange(GatherOptions(symDef.SymbolDefinitions));
+            }
+
+            return optionList;
         }
 
         internal IReadOnlyCollection<string> NormalizeRootCommand(IReadOnlyCollection<string> args)

tmds/command-line-api

src/System.CommandLine.Tests/OptionTests.cs
@@ -162,11 +162,52 @@ namespace System.CommandLine.Tests
                   .Be($"Symbol cannot contain delimiter: \"{delimiter}\"");
         }
 
+        [Fact]
+        public void When_option_argument_is_provided_without_option_name_argument_position_is_assumed()
+        {
+            var result = new CommandLineBuilder()
+                .AddOption("-a", "", a => a.ExactlyOne())
+                .Build()
+                .Parse("value-for-a");
+
+            result.ValueForOption("-a").Should().Be("value-for-a");
+        }
+
+        [Fact]
+        public void When_multiple_option_arguments_are_provided_without_option_name_argument_positions_are_assumed()
+        {
+            var result = new CommandLineBuilder()
+                .AddOption("-a", "", a => a.ExactlyOne())
+                .AddOption("-b", "")
+                .AddOption("-c", "", a => a.ExactlyOne())
+                .Build()
+                .Parse("value-for-a value-for-c");
+
+            result.ValueForOption("-a").Should().Be("value-for-a");
+            result.ValueForOption("-c").Should().Be("value-for-c");
+            result.HasOption("-b").Should().BeFalse();
+        }
+
+        [Fact]
+        public void When_multiple_option_arguments_are_provided_with_first_option_name_argument_positions_are_assumed()
+        {
+            var result = new CommandLineBuilder()
+                .AddOption("-a", "", a => a.ExactlyOne())
+                .AddOption("-b", "")
+                .AddOption("-c", "", a => a.ExactlyOne())
+                .Build()
+                .Parse("-a value-for-a value-for-c");
+
+            result.ValueForOption("-a").Should().Be("value-for-a");
+            result.ValueForOption("-c").Should().Be("value-for-c");
+            result.HasOption("-b").Should().BeFalse();
+        }
+
         [Theory]
         [InlineData("-")]
         [InlineData("--")]
         [InlineData("/")]
-        public void When_option_use_different_prefixes_they_still_work(string prefix)
+        public void When_option_use_differnt_prefixes_they_still_work(string prefix)
         {
             var result = new CommandLineBuilder()
                 .AddOption(prefix + "a", "", a => a.ExactlyOne())
@@ -179,5 +220,38 @@ namespace System.CommandLine.Tests
             result.ValueForOption(prefix + "c").Should().Be("value-for-c");
             result.HasOption(prefix + "b").Should().BeFalse();
         }
-   }
+
+        [Fact]
+        public void When_multiple_option_arguments_are_provided_with_second_option_is_first_positions_are_assumed()
+        {
+            var result = new CommandLineBuilder()
+                .AddOption("-a", "", a => a.ExactlyOne())
+                .AddOption("-b", "")
+                .AddOption("-c", "", a => a.ExactlyOne())
+                .Build()
+                .Parse("-c value-for-c value-for-a");
+
+            result.ValueForOption("-a").Should().Be("value-for-a");
+            result.ValueForOption("-c").Should().Be("value-for-c");
+            result.HasOption("-b").Should().BeFalse();
+        }
+
+        [Fact]
+        public void When_multiple_option_arguments_are_provided_with_b_option_positions_are_assumed()
+        {
+            var result = new CommandLineBuilder()
+                .AddCommand("outer", "")
+                .AddOption("-a", "", a => a.ExactlyOne())
+                .AddOption("-b", "")
+                .AddOption("-c", "", a => a.ExactlyOne())
+                .Build()
+                .Parse("-c value-for-c -b value-for-a outer");
+
+            result.ValueForOption("-a").Should().Be("value-for-a");
+            result.ValueForOption("-c").Should().Be("value-for-c");
+            result.HasOption("-b").Should().BeTrue();
+            //result.Symbols.ElementAt(0).Name.Should().Be("outer");
+        }
+
+    }
 }

src/System.CommandLine/Parser.cs
@@ -30,6 +30,8 @@ namespace System.CommandLine
             Command rootCommand = null;
             Command innermostCommand = null;
 
+            var optionQueue = GatherOptions(Configuration.SymbolDefinitions).ToList();
+
             while (unparsedTokens.Any())
             {
                 var token = unparsedTokens.Dequeue();
@@ -66,9 +68,9 @@ namespace System.CommandLine
 
                 var added = false;
 
-                foreach (var symbol in Enumerable.Reverse(allSymbols))
+                foreach (var topLevelSymbol in Enumerable.Reverse(allSymbols))
                 {
-                    var symbolForToken = symbol.TryTakeToken(token);
+                    Symbol symbolForToken = topLevelSymbol.TryTakeToken(token);
 
                     if (symbolForToken != null)
                     {
@@ -80,12 +82,38 @@ namespace System.CommandLine
                         }
 
                         added = true;
+                        if (token.Type is TokenType.Option)
+                        {
+                            var existing = optionQueue.FirstOrDefault(name => name == symbolForToken.Name);
+                            if (existing != null)
+                            {
+                                // we've used this option - don't use it again
+                                optionQueue.Remove(existing);
+                            }
+                        }
                         break;
                     }
 
                     if (token.Type == TokenType.Argument &&
-                        symbol.SymbolDefinition is CommandDefinition)
+                        topLevelSymbol.SymbolDefinition is CommandDefinition)
                     {
+                        var optionName = optionQueue.FirstOrDefault();
+                        if (optionName != null)
+                        {
+                            optionQueue.RemoveAt(0);
+                            var newToken = new Token("-" + optionName, TokenType.Option);
+                            symbolForToken = topLevelSymbol.TryTakeToken(newToken);
+                            if (symbolForToken != null)
+                            {
+                                allSymbols.Add(symbolForToken);
+                                symbolForToken = topLevelSymbol.TryTakeToken(token);
+                                if (symbolForToken != null)
+                                {
+                                    allSymbols.Add(symbolForToken);
+                                    added = true;
+                                }
+                            }
+                        }
                         break;
                     }
                 }
@@ -110,6 +138,29 @@ namespace System.CommandLine
                 unmatchedTokens,
                 errors,
                 rawInput);
+        }
+
+        private IEnumerable<string> GatherOptions(SymbolDefinitionSet symbolDefinitions)
+        {
+            var optionList = new List<string>();
+            foreach (var symDef in symbolDefinitions) //.Where( s => s is OptionDefinition))
+            {
+                if (symDef is OptionDefinition)
+                {
+                    var validator = symDef.ArgumentDefinition.SymbolValidators.FirstOrDefault();
+                    if (validator != null)
+                    {
+                        if (validator.Method.Name.Contains("ExactlyOne"))
+                        {
+                            optionList.Add(symDef.Name);
+                        }
+                    }
+                }
+
+                optionList.AddRange(GatherOptions(symDef.SymbolDefinitions));
+            }
+
+            return optionList;
         }
 
         internal IReadOnlyCollection<string> NormalizeRootCommand(IReadOnlyCollection<string> args)