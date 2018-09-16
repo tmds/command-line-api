f666ca186688d2cb9404dc3ef087bdedc4da1be9
Kathleen Dollard <kathleen.a.dollard@gmail.com>
2018-09-15 17:33:18 -0700

    Implemented an approach to rootcommand

--- src/System.CommandLine.Tests/SampleDotNetCli/RootCommand.cs
+++ src/System.CommandLine.Tests/SampleDotNetCli/RootCommand.cs
@@ -3,19 +3,43 @@ using System.CommandLine.Tests.SampleDotNetCli.Core;
 
 namespace System.CommandLine.Tests.SampleDotNetCli
 {
-    internal abstract class RootCommand : Core.Command
+    internal class RootCommand : Core.Command
     {
+        // TODO: Let's automate this alias based on capping in the name
         public bool Info { get; set; }
         public bool Version { get; set; }
+        public bool ListRuntimes{ get; set; }
+        public bool ListSdks { get; set; }
 
         [Alias("d")]
         public bool Diagnostics { get; set; }
 
-        // TODO: Let's automate this alias based on capping in the name
-        // [Alias("list-runtimes", true)]
-        public bool ListRuntimes{ get; set; }
-
         // TODO: If we use inheritance, we will need to do something other than abstract to indicate not invokable, currently just this. 
-        public override Task<int> InvokeAsync() => throw new NotImplementedException();
+        public override  Task<int> InvokeAsync()
+        {
+            // TODO: Preserve existing behavior on combined options (error or all or order of use)
+            if (Info)
+            {
+               return Task.FromResult(OutputInfo());
+            }
+            if (Version)
+            {
+                return Task.FromResult(OutputVersion());
+            }
+            if (ListRuntimes)
+            {
+                return Task.FromResult(OutputRuntimes());
+            }
+            if (ListSdks)
+            {
+                return Task.FromResult(OutputSdks());
+            }
+            return Task.FromResult(0);
+        }
+
+        private int OutputSdks() => throw new NotImplementedException();
+        private int OutputRuntimes() => throw new NotImplementedException();
+        private int OutputVersion() => throw new NotImplementedException();
+        private int OutputInfo() => throw new NotImplementedException();
     }
 }
